12447b37263d5f8640d03d33a6747694c9754771
Luis Carrasco <luis@bambucode.com>
2010-11-09 15:17:51 -0700

    EJEMPLO: - Se agrego proyecto de ejemplo en cosola que usa la clase y genera la factura electronica en XML para demostrar lo sencillo que es de usar la clase.

---
+++ Ejemplo/EjemploFacturaElectronica.dpr
@@ -0,0 +1,147 @@
+(******************************************************************************
+ PROYECTO FACTURACION ELECTRONICA
+ Copyright (C) 2010 - Bambu Code SA de CV - Ing. Luis Carrasco
+
+ Proyecto de consola que genera una Factura Electronica de ejemplo
+
+ Este archivo pertenece al proyecto de codigo abierto de Bambu Code:
+ http://bambucode.com/codigoabierto
+
+ La licencia de este codigo fuente se encuentra en:
+ http://github.com/bambucode/bc_facturaelectronica/blob/master/LICENCIA
+ ******************************************************************************)
+program EjemploFacturaElectronica;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  ActiveX,
+  ShlObj,
+  Forms,
+  ClaseOpenSSL in '..\ClaseOpenSSL.pas',
+  ComprobanteFiscal in '..\ComprobanteFiscal.pas',
+  FacturaElectronica in '..\FacturaElectronica.pas',
+  FacturaReglamentacion in '..\FacturaReglamentacion.pas',
+  FacturaTipos in '..\FacturaTipos.pas',
+  libeay32 in '..\libeay32.pas',
+  LibEay32Plus in '..\LibEay32Plus.pas',
+  OpenSSLUtils in '..\OpenSSLUtils.pas',
+  SelloDigital in '..\SelloDigital.pas',
+  FeCFDv2 in '..\CFD\FeCFDv2.pas';
+
+var
+   sArchivo: String;
+   Factura: TFacturaElectronica;
+   Emisor, Receptor: TFEContribuyente;
+   Certificado: TFECertificado;
+   BloqueFolios: TFEBloqueFolios;
+   Impuesto1, Impuesto2: TFEImpuestoTrasladado;
+   Concepto1, Concepto2 : TFEConcepto;
+
+    function GetDesktopFolder: string;
+    var
+     buf: array[0..255] of char;
+     pidList: PItemIDList;
+    begin
+     Result := 'No Desktop Folder found.';
+     SHGetSpecialFolderLocation(Application.Handle, CSIDL_DESKTOP, pidList);
+     if (pidList <> nil) then
+      if (SHGetPathFromIDList(pidList, buf)) then
+        Result := buf;
+    end;
+
+begin
+  {$IF CompilerVersion < 20}
+      // Bajo Delphi < 2009 tenemos que mandar llamar la siguiente rutina
+      // para poder usar rutinas de la clase ActiveX, en este caso la rutina
+      // para obtener la ruta al Escritorio de Windows.
+      CoInitialize(nil);
+  {$IFEND}
+
+
+  try
+      // 1. Definimos los datos del emisor y receptor
+      Emisor.RFC:='AAA010101AAA';
+      Emisor.Nombre:='A.C. de Pruebas';
+      Emisor.Direccion.Calle:='Av. Hidalgo';
+      Emisor.Direccion.NoExterior:='77';
+      Emisor.Direccion.CodigoPostal:='06300';
+      Emisor.Direccion.Colonia:='Guerrero';
+      Emisor.Direccion.Municipio:='Coyoacán';
+      Emisor.Direccion.Estado:='Distrito Federal';
+      Emisor.Direccion.Pais:='México';
+
+      Receptor.RFC:='FIFC000101AM1';
+      Receptor.Nombre:='Contribuyente de Pruebas Ficticio';
+      Receptor.Direccion.Calle:='Av. Hidalgo';
+      Receptor.Direccion.NoExterior:='88';
+      Receptor.Direccion.CodigoPostal:='06301';
+      Receptor.Direccion.Colonia:='Guerrero';
+      Receptor.Direccion.Municipio:='Coyoacán';
+      Receptor.Direccion.Estado:='Distrito Federal';
+      Receptor.Direccion.Pais:='México';
+
+      // 2. Definimos los datos de los folios que nos autorizo el SAT
+      BloqueFolios.NumeroAprobacion:=1;
+      BloqueFolios.AnoAprobacion:=2010;
+      BloqueFolios.Serie:='A';
+      BloqueFolios.FolioInicial:=1;
+      BloqueFolios.FolioFinal:=1000;
+
+      //3. Definimos el certificado junto con su llave privada
+      Certificado.Ruta:=ExtractFilePath(Application.ExeName) + '/aaa010101aaa_CSD_02.cer';
+      Certificado.LlavePrivada.Ruta:=ExtractFilePath(Application.ExeName) + '/aaa010101aaa_CSD_02.key';
+      Certificado.LlavePrivada.Clave:='a0123456789';
+
+      // 4. Creamos la clase Factura con los parametros minimos.
+      Factura:=TFacturaElectronica.Create(Emisor, Receptor, BloqueFolios, Certificado, tcIngreso);
+      //Factura.OnComprobanteGenerado:=onComprobanteGenerado;
+
+      // Definimos todos los conceptos que incluyo la factura
+      Concepto1.Cantidad:=1;
+      Concepto1.Unidad:='PZ';
+      Concepto1.Descripcion:='Lapiz Numero 2';
+      Concepto1.ValorUnitario:=10;
+      Factura.AgregarConcepto(Concepto1);
+
+      // Agregamos el impuesto del concepto 1
+      Impuesto1.Nombre:='IVA';
+      Impuesto1.Tasa:=16;
+           Impuesto1.Importe:=Concepto1.Importe * (Impuesto1.Tasa/100);
+      Factura.AgregarImpuestoTrasladado(Impuesto1);
+
+      Concepto2.Cantidad:=3;
+      Concepto2.Unidad:='M';
+      Concepto2.Descripcion:='Papel para cuaderno por metro';
+      Concepto2.ValorUnitario:=10.50;
+      Factura.AgregarConcepto(Concepto2);
+
+      // Le damos un descuento
+      Factura.AsignarDescuento(5, 'Por pronto pago');
+
+      // Mandamos generar la factura con el siguiente folio disponible
+      sArchivo:=GetDesktopFolder() + '\MiFactura.xml';
+      Factura.GenerarYGuardar(1, fpUnaSolaExhibicion, sArchivo);
+
+      WriteLn('La factura ha sido generada y guardada en el escritorio. El archivo es: ' + sArchivo);
+      Readln;
+      FreeAndNil(Factura);
+  except
+    on E: Exception do
+    begin
+      WriteLn('Ocurrio un error:');
+      Writeln(E.ClassName, ': ', E.Message);
+      ReadLn;
+    end;
+  end;
+
+  {$IF CompilerVersion < 20}
+      CoUnInitialize; // Liberamos la memoria usada por la unidad ActiveX
+  {$IFEND}
+
+end.
+
+
+
+
