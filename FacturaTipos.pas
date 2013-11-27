4fd2e06cfb9c514ca3c55fbea6fe2c6eae1c8c58
lcarrasco <luis@bambucode.com>
2013-11-27 15:36:38 -0700

    Se agregaron excepciones especificas de comunicacion con los PACs.

--- FacturaTipos.pas
+++ FacturaTipos.pas
@@ -1,6 +1,6 @@
 (******************************************************************************
  PROYECTO FACTURACION ELECTRONICA
- Copyright (C) 2010-2012 - Bambu Code SA de CV
+ Copyright (C) 2010-2014 - Bambu Code SA de CV
 
  Define las estructuras usadas como parametros para generar la factura
  electrónica. Pensado por si en el futuro el SAT cambia las estructuras
@@ -16,6 +16,8 @@
 unit FacturaTipos;
 
 interface
+
+uses SysUtils;
 
 type
 
@@ -34,7 +36,9 @@ TFESerie = String[10];
 
 TFEFormaDePago = (fpUnaSolaExhibicion, fpParcialidades);
 TFETipoComprobante = (tcIngreso, tcEgreso, tcTraslado);
-TFEVersionComprobante = (fev20, fev22);
+
+// Versiones de CFD soportadas
+TFEVersionComprobante = (fev20, fev22,fev32);
 
 TFEDireccion = record
   Calle: String;
@@ -68,6 +72,7 @@ TFEContribuyente = record
 	Nombre: String;
 	RFC: TFERFC;
   Direccion: TFeDireccion;
+  ExpedidoEn: TFEExpedidoEn;  
   // Un contribuyente puede tener muchos regímenes fiscales
   Regimenes : TFERegimenes;
 end;
@@ -124,6 +129,18 @@ end;
 
 TFEImpuestosRetenidos = Array of TFEImpuestoRetenido;
 
+TFETimbre = record
+  Version : WideString;
+  UUID : WideString;
+  FechaTimbrado : TDateTime;
+  SelloCFD : WideString;
+  NoCertificadoSAT : WideString;
+  SelloSAT : WideString;
+  XML: WideString;
+end;
+ {
+TTimbreFiscal = Array of TFETimbre;
+}
 TFEImpuestoTrasladado = record
     Nombre: String; // IVA, IEPS
     Tasa: Double;
@@ -135,9 +152,75 @@ TFEImpuestosTrasladados = Array of TFEImpuestoTrasladado;
 // Usado por la clase ReporteMensual
 TFEPedimentos = Array of TFEDatosAduana;
 
+TOnTimbradoRecibidoEvent = procedure(Sender: TObject; const aTimbre: TFETimbre) of Object;
+
+TFEPACCredenciales = record
+  RFC: string;
+  Clave: String;
+  DistribuidorID: string; // Usado por algunos PAC para la comunicacion (Ej: Ecodex)
+end;
+
+// Excepciones que se lanzan durante el proceso de timbrado de un CFDI
+
+{$REGION 'Documentation'}
+///	<summary>
+///	  Error estándard del SAT (301) el cual se lanza cuando el XML enviado al
+///	  PAC no fue válido.
+///	</summary>
+{$ENDREGION}
+ETimbradoXMLInvalidoException = class(Exception);
+
+{$REGION 'Documentation'}
+///	<summary>
+///	  Error estándard del SAT (302) cuando el sello del comprobante no se
+///	  generó correctamente por algún error en el comprobante o el cálculo del
+///	  sello.
+///	</summary>
+{$ENDREGION}
+ETimbradoSelloEmisorInvalidoException = class(Exception); // Error 302
+ETimbradoCertificadoNoCorrespondeException = class(Exception); // Error 303
+ETimbradoCertificadoRevocadoException = class(Exception); // Error 304
+ETimbradoFechaEmisionSinVigenciaException = class(Exception); // Error 305
+ETimbradoLlaveInvalidaException = class(Exception); // Error 306
+ETimbradoPreviamenteException = class(Exception); // Error 307
+ETimbradoCertificadoApocrifoException = class(Exception); // Error 308
+ETimbradoFechaGeneracionMasDe72HorasException = class(Exception); // Error 401
+ETimbradoRegimenEmisorNoValidoException = class(Exception); // Error 402
+ETimbradoFechaEnElPasadoException = class(Exception); // Error 403
+
+// Excepciones comunes para todos los PAC
+
+{$REGION 'Documentation'}
+///	<summary>
+///	  Representa una "caida" general del servicio del PAC. Es decir
+///	  temporalmente fuera de servicio
+///	</summary>
+{$ENDREGION}
+EPACServicioNoDisponibleException = class(Exception);
+
+{$REGION 'Documentation'}
+///	<summary>
+///	  Excepcion general para errores no programados/manejados.
+///	</summary>
+{$ENDREGION}
+ETimbradoErrorGenericoException = class(Exception);
+
 const
-	_RFC_VENTA_PUBLICO_EN_GENERAL = 'XAXX010101000';
-	_RFC_VENTA_EXTRANJEROS        = 'XEXX010101000';
+_RFC_VENTA_PUBLICO_EN_GENERAL = 'XAXX010101000';
+_RFC_VENTA_EXTRANJEROS        = 'XEXX010101000';
+
+// Códigos de error regresados por los PAC
+_ERROR_SAT_XML_INVALIDO                   = '301';
+_ERROR_SAT_SELLO_EMISOR_INVALIDO          = '302';
+_ERROR_SAT_CERTIFICADO_NO_CORRESPONDE     = '303';
+_ERROR_SAT_CERTIFICADO_REVOCADO           = '304';
+_ERROR_SAT_FECHA_EMISION_SIN_VIGENCIA     = '305';
+_ERROR_SAT_LLAVE_NO_CORRESPONDE           = '306';
+_ERROR_SAT_PREVIAMENTE_TIMBRADO           = '307';
+_ERROR_SAT_CERTIFICADO_NO_FIRMADO_POR_SAT = '308';
+_ERROR_SAT_FECHA_FUERA_DE_RANGO           = '401';
+_ERROR_SAT_REGIMEN_EMISOR_NO_VALIDO       = '402';
+_ERROR_SAT_FECHA_EMISION_EN_EL_PASADO     = '403';
 
 implementation
 
