5c14ac0f1bb57688f08217d6652194bcb6776160
ptorres <ptorres99@macpt.lan>
2013-12-04 23:55:38 -0700

    Correccion
    Se corrigieron errores para cuando delphi no tiene instalado el
    codesite,se corrigieron errores al validar las librerias en el folder

--- PACs/FinkOk/PACFinkOk.pas
+++ PACs/FinkOk/PACFinkOk.pas
@@ -67,9 +67,8 @@ end;
 
 implementation
 
-uses {$IF Compilerversion >= 20} Soap.InvokeRegistry, {$IFEND}
-     CodeSiteLogging;
-
+{$IF Compilerversion >= 20}uses  Soap.InvokeRegistry {$IFEND}
+  {$IFDEF CODESITE},CodeSiteLogging {$ENDIF} 
 function TPACFinkOk.UTF8Bytes(const s: UTF8String): TBytedynArray; // sacada de http://stackoverflow.com/questions/5233480/string-to-byte-array-in-utf-8
 begin
 {$IF Compilerversion >= 20}Assert(StringElementSize(s)=1){$IFEND};
@@ -109,7 +108,11 @@ begin
   if respuestaTimbrado.success then
     Result:=respuestaTimbrado.message
   Else
-   CodeSite.Send('El Cliente no se pudo agregar '+respuestaTimbrado.message);
+  {$IFDEF CODESITE}
+    CodeSite.Send('El Cliente no se pudo agregar '+respuestaTimbrado.message);
+  {$ELSE}
+   raise exception.Create('El Cliente no se pudo agregar '+respuestaTimbrado.message);
+  {$ENDIF}
 End;
 
 Function TPACFinkOk.EditaCliente(const Activar: Boolean;const aRFC:String):String;
@@ -124,7 +127,12 @@ begin
   if respuestaTimbrado.success then
     Result:=respuestaTimbrado.message
   Else
+  {$IFDEF CODESITE}
    CodeSite.Send('El Cliente no se pudo modificar '+respuestaTimbrado.message);
+  {$ELSE}
+   raise exception.Create('El Cliente no se pudo modificar '+respuestaTimbrado.message);
+  {$ENDIF}
+
 End;
 
 Function TPACFinkOk.BorraCliente(const aRFC: String):String;
@@ -135,7 +143,11 @@ begin
   if respuestaTimbrado.success then
     Result:=respuestaTimbrado.message
   Else
+  {$IFDEF CODESITE}
    CodeSite.Send('El Cliente no se pudo eliminar '+respuestaTimbrado.message);
+  {$ELSE}
+   raise exception.Create('El Cliente no se pudo eliminar '+respuestaTimbrado.message);
+  {$ENDIF}
 End;
 
 function TPACFinkOk.ObtenerURLTimbrado: String;
@@ -224,8 +236,14 @@ begin
       begin
        if Trim(respuestaTimbrado.CodEstatus) = '' then
         begin
+        {$IFDEF CODESITE}
          CodeSite.Send('Falla Validacion Error No.', respuestaTimbrado.Incidencias[0].CodigoError);
          CodeSite.Send('Falla Validacion Desc:', respuestaTimbrado.Incidencias[0].MensajeIncidencia);
+        {$ELSE}
+         raise exception.Create('Falla Validacion Error No.'+ respuestaTimbrado.Incidencias[0].CodigoError);
+         raise exception.Create('Falla Validacion Desc:'+ respuestaTimbrado.Incidencias[0].MensajeIncidencia);
+       {$ENDIF}
+
         end;
         end else
           raise;
