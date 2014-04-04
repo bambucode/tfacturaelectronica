efdef3643395cf7c42e05aef2f20b52b3d468db6
Ing Eduardo Padilla <epadillac@gmail.com>
2014-04-04 12:43:16 -0700

    Se mejora codigo para detectar errores conocidos.

--- PACs/ManejadorDeErroresComunes.pas
+++ PACs/ManejadorDeErroresComunes.pas
@@ -42,6 +42,8 @@ begin
       CodeSite.SendException(aExcepcion);
     {$ENDIF}
 
+    DetectarErroresConocidos(aExcepcion);
+
     if aExcepcion Is ESOAPHttpException then
     begin
       // Lista de codigos de error estandares:
@@ -49,11 +51,8 @@ begin
       case ESOAPHttpException(aExcepcion).StatusCode of
         502, 503, 504, 522: raise EPACProblemaConInternetException.Create('No se pudo realizar una conexion con el PAC: ' +
                                                            aExcepcion.Message, 0, 0, True);
-      else
-        DetectarErroresConocidos(aExcepcion)
       end;
-    end else
-      DetectarErroresConocidos(aExcepcion);
+    end;
   end;
 end;
 
