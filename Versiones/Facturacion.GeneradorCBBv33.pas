7ddd040a7b56e7187b9b877df6b578700f67f7b8
Luis Carrasco <luis@bambucode.com>
2017-05-18 10:45:29 -0600

    Se crea interfase y clase para generar el CBB de CFDI v3.3

---
+++ Versiones/Facturacion.GeneradorCBBv33.pas
@@ -0,0 +1,72 @@
+unit Facturacion.GeneradorCBBv33;
+
+interface
+
+uses System.SysUtils,
+     Facturacion.Comprobante,
+     Facturacion.GeneradorCBB,
+     Facturacion.ComprobanteV33,
+     Facturacion.GeneradorQR;
+
+type
+
+  TGeneradorCBBv33 = class(TInterfacedObject, IGeneradorCBB)
+  private
+    fGeneradorQR: TGeneradorQR;
+  public
+    destructor Destroy; override;
+    procedure AfterConstruction; override;
+    procedure GenerarImagenCBB(const aComprobante: IComprobanteFiscal;
+                               const aRutaAGuardar: TFileName);
+  end;
+
+
+implementation
+
+{ TGeneradorCBBv33 }
+
+procedure TGeneradorCBBv33.AfterConstruction;
+begin
+  inherited;
+  fGeneradorQR := TGeneradorQR.Create;
+end;
+
+destructor TGeneradorCBBv33.Destroy;
+begin
+  fGeneradorQR.Free;
+
+  inherited;
+end;
+
+procedure TGeneradorCBBv33.GenerarImagenCBB(const aComprobante: IComprobanteFiscal;
+  const aRutaAGuardar: TFileName);
+var
+  cadenaParaCBB, uuid, selloParcial : string;
+  totalComprobante : Currency;
+  facturaV33 : IComprobanteFiscalV33;
+const
+  _FORMATO_CBB = 'https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx&id=%s&re=%s&rr=%s&tt=%d&fe=%s';
+begin
+  if Not Supports(aComprobante, IComprobanteFiscalV33, facturaV33) then
+    raise Exception.Create('Se intentó generar un CBB de CFDI 3.3 de un comprobante que no es de dicha versión');
+
+  // Ocho últimos caracteres del sello digital del emisor del comprobante.
+  selloParcial := Copy(aComprobante.Sello, Length(aComprobante.Sello) - 8,
+                       8);
+
+  uuid := aComprobante.UUID;
+  totalComprobante := StrToCurr(facturaV33.Total);
+
+  cadenaParaCBB := Format(_FORMATO_CBB,
+                          [uuid,
+                           facturaV33.Emisor.RFC,
+                           facturaV33.Receptor.RFC,
+                           // Total del comprobante máximo a 25 posiciones (18 para los enteros, 1 para carácter “.”, 6 para los decimales),
+                           // se deben omitir los ceros no significativos, precedido por el texto “&tt=”
+                           FloatToStrF(totalComprobante, ffFixed, 17, 6),
+                           selloParcial]);
+
+  fGeneradorQR.GenerarQRCode(cadenaParaCBB, aRutaAGuardar);
+end;
+
+end.
