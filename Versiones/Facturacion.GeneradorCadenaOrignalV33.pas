bf9982a640a2fbf3dba182a58c34a8a8a6f6ef77
Francisco Armando Dueñas Rodriguez <fduenas@gmail.com>
2018-09-11 17:57:24 -0500

    Facturacion.GeneradorCadenaOrignalV33.pas:  - se transfirió la función de eliminar el nodo 'Leyenda' a la clase TimbreFiscalDigialV33 en Facturacion.TimbreFiscalDigitalV33.pas

--- Versiones/Facturacion.GeneradorCadenaOrignalV33.pas
+++ Versiones/Facturacion.GeneradorCadenaOrignalV33.pas
@@ -79,7 +79,6 @@ var
   contenidoXSLTCadenaOriginal: TCadenaUTF8;
   transformador: TTransformadorDeXML;
   facturaV33: IComprobanteFiscalV33;
-  nodoLeyenda: IXMLNode;
 begin
   Assert(aComprobante <> nil, 'La instancia aComprobante no debio ser nula');
   // Verificamos que la versión del comprobante sea 3.3 pues solo será una cadena original válida para dicha versión
@@ -94,27 +93,6 @@ begin
 
   transformador := TTransformadorDeXML.Create;
   try
-    // Existe un error en el XSLT del Timbre fiscal donde si el Nodo Leyenda se auto-agregó al final,
-    //  Ej: cuando se consulta el valor de 'TimbreFiscalDigitalV33.Leyenda', entonces
-    // la transformación incluirá un CHR(10)+Chr(13) o #$D#$A entre el RFC y la Leyenda.
-
-    //Ejemplo:
-    // Sin Leyenda              : '|1.1|4ab11a49-cdcf-4e42-8278-3b89219431ca|2018-09-11T15:53:23|AAA010101AAA|ghabiW....Bxl7Q==|20001000000300022323'
-    // Con Leyenda ('' ó 'XX..'): '|1.1|4ab11a49-cdcf-4e42-8278-3b89219431ca|2018-09-11T15:53:23|AAA010101AAA'+'#$D#$A'+'||ghabiW....Bxl7Q==|20001000000300022323'
-
-    // Por la tanto si el nodo de la Leyenda se auto-agregó después del timbrado,
-    // este se eliminará si el nodo está vacío para evitar ese error
-
-    nodoLeyenda := facturaV33.Complemento.TimbreFiscalDigital.AttributeNodes.FindNode('Leyenda');
-    if Assigned( nodoLeyenda ) and
-      (trim(nodoLeyenda.Text)='') and
-      (facturaV33.Complemento.TimbreFiscalDigital.AttributeNodes.IndexOf('Leyenda') =
-      (facturaV33.Complemento.TimbreFiscalDigital.AttributeNodes.Count-1) ) Then
-    begin
-     {Si La posición de Leyenda es la última de la lista de nodos, quiere decir que se agregó después del timbrado}
-     facturaV33.Complemento.TimbreFiscalDigital.AttributeNodes.Delete('Leyenda');
-    end;
-
     contenidoXMLComprobante := facturaV33.Complemento.TimbreFiscalDigital.XML;
     contenidoXSLTCadenaOriginal :=
       transformador.obtenerXSLTDeRecurso(_NOMBRE_RECURSO_CADENA_ORIGINAL_TFD);
