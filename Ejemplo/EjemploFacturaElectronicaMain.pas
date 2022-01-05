6bf92178b8329816c098b0071fedfac1872d4c64
Luis Carrasco <luis@bambucode.com>
2022-01-05 15:06:25 -0700

    Se mejora proyecto de Ejemplo para generar v3.3 o v4.0.
    - Se remueve generacion de CFDI 3.2 pues ya es obsoleta (Dic 2017)

--- Ejemplo/EjemploFacturaElectronicaMain.pas
+++ Ejemplo/EjemploFacturaElectronicaMain.pas
@@ -24,14 +24,14 @@ uses
   Facturacion.PAC.Comercio,
   Facturacion.ProveedorAutorizadoCertificacion, Facturacion.OpenSSL,
   Facturacion.GeneradorCBB,
-  Facturacion.ComprobanteV32,
-  Facturacion.GeneradorCadenaOrignalV32,
-  Facturacion.GeneradorSelloV32,
+
+  // CFDI 3.3
   Facturacion.GeneradorCBBv32,
   Facturacion.ComprobanteV33,
-  //Facturacion.GeneradorCadenaOrignalV33,
+  Facturacion.GeneradorCadenaOrignalV33,
   Facturacion.GeneradorSelloV33,
   Facturacion.GeneradorCBBv33,
+
   // CFDI 4.0:
   Facturacion.ComprobanteV40,
   Facturacion.GeneradorCadenaOrignalV40,
@@ -47,17 +47,16 @@ implementation
  var
   nuevaFactura                                                    : IComprobanteFiscal;
 
-  // Instancias especificas para V32
-  facturaCFDIv32                                                  : IComprobanteFiscalV32;
-  regimen32                                                       : IComprobanteFiscalV32_Emisor_RegimenFiscal;
-  concepto32                                                      : IComprobanteFiscalV32_Conceptos_Concepto;
-  iva32                                                           : IComprobanteFiscalV32_Impuestos_Traslados_Traslado;
+  facturaCFDIv33                                                  : IComprobanteFiscalv33;
+  concepto33                                                      : IComprobanteFiscalv33_Conceptos_Concepto;
+  iva33                                                           : IComprobanteFiscalv33_Conceptos_Concepto_Impuestos_Traslados_Traslado;
+  totalIVA33                                                      : IComprobanteFiscalv33_Impuestos_Traslados_Traslado;
 
   // Instancias especificas para v40
   facturaCFDIv40                                                  : IComprobanteFiscalv40;
-  concepto33                                                      : IComprobanteFiscalv40_Conceptos_Concepto;
-  iva33                                                           : IComprobanteFiscalv40_Conceptos_Concepto_Impuestos_Traslados_Traslado;
-  totalIVA33                                                      : IComprobanteFiscalv40_Impuestos_Traslados_Traslado;
+  concepto40                                                      : IComprobanteFiscalv40_Conceptos_Concepto;
+  iva40                                                           : IComprobanteFiscalv40_Conceptos_Concepto_Impuestos_Traslados_Traslado;
+  totalIVA40                                                      : IComprobanteFiscalv40_Impuestos_Traslados_Traslado;
 
   // Instancia de impuestos locales v1.0 (compatible con CFDI 3.2 y 3.3)
   impuestoLocalV1                                                 : IImpuestosLocalesV1;
@@ -80,7 +79,7 @@ implementation
   Url_WS                                                          : String;
   cadenaOriginal, selloDeLaFactura : String;
  const
-  _URL_ECODEX_PRUEBAS_V32        = 'https://pruebas.ecodex.com.mx:2045';
+  _URL_ECODEX_PRUEBAS_V33        = 'https://pruebas.ecodex.com.mx:2045';
   _URL_ECODEX_PRUEBAS_v40        = 'https://pruebas-wsdex.ecodex.com.mx';
   _URL_FINKOK_PRUEBAS            = 'http://demo-facturacion.finkok.com/servicios/soap';
   _URL_COMERCIO_PRUEBAS          = 'https://pruebas.comercio-digital.mx';
@@ -186,9 +185,9 @@ implementation
       WriteLn('Certificado y llave ... OK');
 
 
-      queVersion := '4.0';
-      //Writeln('Por favor escribe la version del CFDI que deseas generar (3.2 o 3.3):');
-      //ReadLn(queVersion);
+      //queVersion := '4.0';
+      Writeln('Por favor escribe la version del CFDI que deseas generar (3.3 o 4.0):');
+      ReadLn(queVersion);
       Writeln;
 
       Writeln('Creando instancia de PAC...');
@@ -204,77 +203,92 @@ implementation
           nuevaFactura := admonFacturas.Nueva(queVersion);
           Writeln('Generando CFDI v' + nuevaFactura.Version + '...');
 
-          // Checamos que la interfase sea de la version correcta
-          if nuevaFactura.Version = '3.2' then
+
+          {$REGION 'CFDI 3.3'}
+          if nuevaFactura.Version = '3.3' then
           begin
-            if Not Supports(nuevaFactura, IComprobanteFiscalv32, facturaCFDIv32) then
+            if Not Supports(nuevaFactura, IComprobanteFiscalv33, facturaCFDIv33) then
             begin
-              Writeln('nuevaFactura no fue un Comprobante Fiscal v32');
+              Writeln('nuevaFactura no fue un Comprobante Fiscal v33');
               Exit;
             end;
 
-            {$IFDEF undef}{$REGION 'Factura V32'}{$ENDIF}
-            // Creamos las instancias correspondientes para la v40
-            generadorCadena := TGeneradorCadenaOriginalV32.Create;
-            generadorSello := TGeneradorSelloV32.Create;
-            generadorSello.Configurar(openSSL);
-            generadorCBB  := TGeneradorCBBv32.Create;
 
-            with facturaCFDIv32 do
+            // Creamos las instancias correspondientes para la v33
+            generadorCadena := TGeneradorCadenaOriginalv33.Create;
+            generadorSello := TGeneradorSellov33.Create;
+            generadorSello.Configurar(openSSL);
+            generadorCBB  := TGeneradorCBBv33.Create;
+
+            Writeln('Llenando comprobante CFDI v3.3...');
+            with facturaCFDIv33 do
             begin
-              Serie     := 'Ver32';
+              Serie     := 'Ver33';
               Randomize;
               Folio     := IntToStr(Random(999999999));
               Fecha     := TFacturacionHelper.ComoFechaISO8601(Now);
 
               NoCertificado := certificadoSellos.NoCertificado;
               Certificado   := certificadoSellos.ContenidoBase64;
-
-              FormaDePago       := '01';
-              CondicionesDePago := 'Credito a 30 dias';
-              Subtotal          := '100.00';
-              Descuento         := '0.00';
-              Total             := '116.00';
-              TipoDeComprobante := 'ingreso';
-              MetodoDePago      := '01';
-              LugarExpedicion   := 'Chihuahua, Chihuahua';
+              FormaPago         := '01'; // De catálogo
+              CondicionesDePago := 'Crédito a 30 días';
+              Subtotal          := '100.00'; // Solo 2 decimales
+              Descuento         := TFacturacionHelper.ComoMoneda(0);
+              Moneda            := 'MXN'; // De catálogo
+              TipoCambio        := '1';//TFacturacionHelper.ComoMoneda(1);
+              Total             := TFacturacionHelper.ComoMoneda(116);
+              TipoDeComprobante := 'I'; // De catálogo
+              MetodoPago        := 'PUE';
+              LugarExpedicion   := '76030';
 
               Emisor.Rfc           := certificadoSellos.EmitidoParaRFC;
               Emisor.Nombre        := certificadoSellos.EmitidoParaNombre;
+              Emisor.RegimenFiscal := '612'; // De catálogo
 
-              regimen32 := Emisor.RegimenFiscal.Add;
-              regimen32.Regimen := 'Regimen General de Ley';
+              Receptor.Rfc              := Uppercase('cacx7605101p8');
+              Receptor.Nombre           := 'XOTICHL CASAS CHAVEZ';
+              Receptor.UsoCFDI          := 'G01';
 
-              Receptor.Rfc  := 'MTI0806042N7';
-              Receptor.Nombre := 'Juan & José & ''Niño'' & "Niña" S.A. de C.V.';
-              Receptor.Domicilio.Calle := 'Mi Calle';
-              Receptor.Domicilio.NoExterior := '120';
-              Receptor.Domicilio.Colonia := 'Centro';
-              Receptor.Domicilio.Municipio := 'Chihuahua';
-              Receptor.Domicilio.Estado := 'Chihuahua';
-              Receptor.Domicilio.Pais := 'México';
+              //Receptor.ResidenciaFiscal := 'USA'; // De catálogo
+              // Solo para cliente extranjero
+              //Receptor.NumRegIdTrib     := '123456789'; // "formatoDeRegistroDeIdentidadTributaria": "[0-9]{9}",
 
-              concepto32 := Conceptos.Add;
-              concepto32.NoIdentificacion := '1';
-              concepto32.Cantidad         := '1';
-              concepto32.Unidad           := 'PZA';
-              concepto32.Descripcion      := 'Concepto No 1';
-              concepto32.ValorUnitario    := '100.00';
-              concepto32.Importe          := '100.00';
+              concepto33 := Conceptos.Add;
+              concepto33.ClaveProdServ    := '52161529';  // De catálogo
+              concepto33.NoIdentificacion := '1';
+              concepto33.Cantidad         := '1';
+              concepto33.ClaveUnidad      := 'EA';  // De catálogo
+              concepto33.Unidad           := 'PZA'; // De catálogo
+              concepto33.Descripcion      := 'Concepto No 1';
+              concepto33.ValorUnitario    := '100.00';
+              concepto33.Importe          := '100.00';
+              concepto33.Descuento        := '0.00';
 
-              // Asignamos el total del impuesto de la factura
-              iva32 := Impuestos.Traslados.Add;
-              iva32.Tasa        := '16.00';
-              iva32.Impuesto    := 'IVA';
-              iva32.Importe     := '16.00';
+              iva33 := concepto33.Impuestos.Traslados.Add;
+              iva33.Base        := '100.00';
+              iva33.Impuesto    := '002';
+              iva33.TipoFactor  := 'Tasa';
+              iva33.TasaOCuota  := '0.160000';
+              iva33.Importe     := '16.00';
+
+              // NOTA: Agregamos el numero cuenta predial justo después de indicar
+              // los impuestos del concepto pues el orden importa.
+              //concepto33.CuentaPredial.Numero := '234989';
 
               Impuestos.TotalImpuestosTrasladados  := '16.00';
 
-            end;
-            {$IFDEF undef}{$ENDREGION}{$ENDIF}
+              totalIVA33 := Impuestos.Traslados.Add;
+              totalIVA33.Impuesto := '002';
+              totalIVA33.TipoFactor := 'Tasa';
+              totalIVA33.TasaOCuota := '0.160000';
+              totalIVA33.Importe    := '16.00';
 
+            end;
           end;
 
+          {$ENDREGION}
+
+          {$REGION 'CFDI 4.0'}
           if nuevaFactura.Version = '4.0' then
           begin
             if Not Supports(nuevaFactura, IComprobanteFiscalv40, facturaCFDIv40) then
@@ -327,25 +341,25 @@ implementation
               // Solo para cliente extranjero
               //Receptor.NumRegIdTrib     := '123456789'; // "formatoDeRegistroDeIdentidadTributaria": "[0-9]{9}",
 
-              concepto33 := Conceptos.Add;
-              concepto33.ClaveProdServ    := '52161529';  // De catálogo
-              concepto33.NoIdentificacion := '1';
-              concepto33.Cantidad         := '1';
-              concepto33.ClaveUnidad      := 'EA';  // De catálogo
-              concepto33.Unidad           := 'PZA'; // De catálogo
-              concepto33.Descripcion      := 'Concepto No 1';
-              concepto33.ValorUnitario    := '100.00';
-              concepto33.Importe          := '100.00';
-              concepto33.Descuento        := '0.00';
+              concepto40 := Conceptos.Add;
+              concepto40.ClaveProdServ    := '52161529';  // De catálogo
+              concepto40.NoIdentificacion := '1';
+              concepto40.Cantidad         := '1';
+              concepto40.ClaveUnidad      := 'EA';  // De catálogo
+              concepto40.Unidad           := 'PZA'; // De catálogo
+              concepto40.Descripcion      := 'Concepto No 1';
+              concepto40.ValorUnitario    := '100.00';
+              concepto40.Importe          := '100.00';
+              concepto40.Descuento        := '0.00';
 
-              concepto33.ObjetoImp        := '02';
+              concepto40.ObjetoImp        := '02';
 
-              iva33 := concepto33.Impuestos.Traslados.Add;
-              iva33.Base        := '100.00';
-              iva33.Impuesto    := '002';
-              iva33.TipoFactor  := 'Tasa';
-              iva33.TasaOCuota  := '0.160000';
-              iva33.Importe     := '16.00';
+              iva40 := concepto40.Impuestos.Traslados.Add;
+              iva40.Base        := '100.00';
+              iva40.Impuesto    := '002';
+              iva40.TipoFactor  := 'Tasa';
+              iva40.TasaOCuota  := '0.160000';
+              iva40.Importe     := '16.00';
 
               // NOTA: Agregamos el numero cuenta predial justo después de indicar
               // los impuestos del concepto pues el orden importa.
@@ -353,16 +367,18 @@ implementation
 
               Impuestos.TotalImpuestosTrasladados  := '16.00';
 
-              totalIVA33 := Impuestos.Traslados.Add;
-              totalIVA33.Base     := '100';
-              totalIVA33.Impuesto := '002';
-              totalIVA33.TipoFactor := 'Tasa';
-              totalIVA33.TasaOCuota := '0.160000';
-              totalIVA33.Importe    := '16.00';
+              totalIVA40 := Impuestos.Traslados.Add;
+              totalIVA40.Base     := '100';
+              totalIVA40.Impuesto := '002';
+              totalIVA40.TipoFactor := 'Tasa';
+              totalIVA40.TasaOCuota := '0.160000';
+              totalIVA40.Importe    := '16.00';
 
             end;
             {$IFDEF undef}{$ENDREGION}{$ENDIF}
           end;
+
+          {$ENDREGION}
 
           // Agregamos el impuesto local el cual se maneja de forma especial
           {$IFDEF undef}{$REGION 'Impuestos locales'}{$ENDIF}
@@ -381,7 +397,7 @@ implementation
           {$IFDEF undef}{$ENDREGION}{$ENDIF}
 
           admonFacturas.GuardarArchivo(nuevaFactura,
-                                      ExtractFilePath(Application.ExeName) + '\ejemplo-cfdi4.xml');
+                                      ExtractFilePath(Application.ExeName) + '\ejemplo-cfdi.xml');
 
           // Obtenemos la cadena original y sellamos la factura automaticamente
           Writeln('Sellando comprobante...');
@@ -418,7 +434,7 @@ implementation
           begin
            pac.AsignarParametro(PAC_PARAM_SVC_CFDI_VERSION, PAC_VALOR_CFDI_VERSION_32);
            {$ifdef PAC_DEMO_ECODEX}
-             Url_WS := _URL_ECODEX_PRUEBAS_V32;
+             Url_WS := _URL_ECODEX_PRUEBAS_V33;
            {$endif}
           end;
 
@@ -472,7 +488,7 @@ implementation
         Writeln(' Fecha del Timbre (Conversión): '+ FormatDateTime('yyyy-mm-dd"T"HH:nn:ss', TFacturacionHelper.DesdeFechaISO8601( facturaCFDIv40.Complemento.TimbreFiscalDigital.FechaTimbrado )) );
        end;
        //Agregamos una Addenda de ejemplo
-       WriteLn('Agregando Addenda CFDI v3.3...');
+       WriteLn('Agregando Addenda CFDI v4.0...');
        facturaCFDIv40.Addenda.AddChild('Ejemplo_Addenda').Attributes['observaciones'] := 'Linea 01'+sLineBreak+'Linea 02';
       end;
 
