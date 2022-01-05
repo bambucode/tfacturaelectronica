2d5e760c3e061ae4e8300766b96da34ee48184cd
Luis Carrasco <luis@bambucode.com>
2022-01-05 14:06:42 -0700

    Se corrige generación de CBB para soportar CFDI v4.0

--- Facturacion.GeneradorLigaVerificacion.pas
+++ Facturacion.GeneradorLigaVerificacion.pas
@@ -9,7 +9,8 @@ uses
      SysUtils,
 {$IFEND}
      Facturacion.Comprobante,
-     Facturacion.ComprobanteV33;
+     Facturacion.ComprobanteV33,
+     Facturacion.ComprobanteV40;
 
 type
 
@@ -36,25 +37,42 @@ var
   uuid, selloParcial : string;
   totalComprobante : Currency;
   facturaV33 : IComprobanteFiscalV33;
+  facturaV40 : IComprobanteFiscalV40;
+  rfcEmisor, rfcReceptor: String;
 const
   _FORMATO_CBB    = 'https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?id=%s&re=%s&rr=%s&tt=%s&fe=%s';
   _LONGITUD_SELLO = 8;
 begin
   if Not Supports(aComprobante, IComprobanteFiscalV33, facturaV33) then
-    raise Exception.Create('Se intentó generar una liga de verificacion de CFDI 3.3 de un comprobante que no es de dicha versión');
+  begin
+    if Not Supports(aComprobante, IComprobanteFiscalV40, facturaV40) then
+      raise Exception.Create('Se intentó generar una liga de verificacion de CFDI 3.3/4.0 de un comprobante que no es de estas versiones');
+  end;
 
-  Assert(facturaV33.Complemento.TimbreFiscalDigital <> nil, 'Se mando un CFDI no timbrado');
+  if Assigned(facturaV40) then
+  begin
+    uuid             := Lowercase(facturaV40.Complemento.TimbreFiscalDigital.UUID);
+    totalComprobante := TFacturacionHelper.DesdeMoneda(facturaV40.Total);
+    rfcEmisor := facturaV40.Emisor.RFC;
+    rfcReceptor := facturaV40.Receptor.RFC;
+  end else
+  begin
+    // Version 3.3
+    Assert(facturaV33.Complemento.TimbreFiscalDigital <> nil, 'Se mando un CFDI no timbrado');
+    uuid             := Lowercase(facturaV33.Complemento.TimbreFiscalDigital.UUID);
+    totalComprobante := TFacturacionHelper.DesdeMoneda(facturaV33.Total);
+    rfcEmisor := facturaV33.Emisor.RFC;
+    rfcReceptor := facturaV33.Receptor.RFC;
+  end;
 
   // Ocho últimos caracteres del sello digital del emisor del comprobante.
   selloParcial     := Copy(aComprobante.Sello, Length(aComprobante.Sello) - _LONGITUD_SELLO + 1,
                            _LONGITUD_SELLO);
-  uuid             := Lowercase(facturaV33.Complemento.TimbreFiscalDigital.UUID);
-  totalComprobante := TFacturacionHelper.DesdeMoneda(facturaV33.Total);
 
   Result := Format(_FORMATO_CBB,
                   [uuid,
-                   facturaV33.Emisor.RFC,
-                   facturaV33.Receptor.RFC,
+                   rfcEmisor,
+                   rfcReceptor,
                    // Total del comprobante máximo a 25 posiciones (18 para los enteros, 1 para carácter “.”, 6 para los decimales),
                    // se deben omitir los ceros no significativos, precedido por el texto “&tt=”
                    FloatToStrF(totalComprobante, ffFixed, 17, 6, TFacturacionHelper.ObtenerConfiguracionRegionalLocal),
